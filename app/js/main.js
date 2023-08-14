3deb94d9849a3f0bfe247eb92680247e0d918426
Denys <den.popov1002@gmail.com>
2023-08-14 17:30:15 +0300

    changes

--- app/js/main.js
+++ app/js/main.js
@@ -575,11 +575,11 @@ const relatedSwiper = new Swiper(".related__swiper", {
 
 function headerFixed() {
   const scrollTop = document.documentElement.scrollTop;
-  headerWrapper.classList.toggle('sticky', scrollTop >= 100);
-  headerWrapper.classList.toggle('animation', scrollTop >= 200);
-  headerWrapper.classList.toggle('opacity', scrollTop >= 350);
+  headerWrapper.classList.toggle("sticky", scrollTop >= 100);
+  headerWrapper.classList.toggle("animation", scrollTop >= 200);
+  headerWrapper.classList.toggle("opacity", scrollTop >= 350);
 }
-window.addEventListener('scroll',  headerFixed);
+window.addEventListener("scroll", headerFixed);
 
 function changeHeaderTextColor() {
   if (document.querySelector(".breadcrumbs") !== null) {
@@ -650,43 +650,27 @@ document.addEventListener("DOMContentLoaded", function () {
   const headerHeight = headerBox.offsetHeight;
   const scrollLinks = document.querySelectorAll(".scroll");
 
-  const bodyStyles = window.getComputedStyle(document.body);
-  const zoomValue = parseFloat(bodyStyles.zoom);
-  console.log(zoomValue);
+  // const bodyStyles = window.getComputedStyle(document.body);
+  // const zoomValue = parseFloat(bodyStyles.zoom);
   scrollLinks.forEach(function (link) {
     link.addEventListener("click", function (event) {
       event.preventDefault();
-
       const scrollAnchor = this.getAttribute("href");
       const scrollTarget = document.querySelector(scrollAnchor);
-
       if (scrollTarget) {
         const scrollPoint =
-          scrollTarget.getBoundingClientRect().top - headerHeight;
+          scrollTarget.getBoundingClientRect().top +
+          window.scrollY -
+          headerHeight;
         let additionalOffset = 0;
-        if (zoomValue === 0.8 && scrollAnchor === "#contact") {
-          additionalOffset = 900;
-        } else if (zoomValue === 0.8 && scrollAnchor === "#new-products") {
-          additionalOffset = 500;
-        } else if (zoomValue === 0.8 && scrollAnchor === "#question") {
-          additionalOffset = 1100;
-        }
-        if (zoomValue === 0.9 && scrollAnchor === "#contact") {
-          additionalOffset = 300;
-        } else if (zoomValue === 0.9 && scrollAnchor === "#new-products") {
-          additionalOffset = 200;
-        } else if (zoomValue === 0.9 && scrollAnchor === "#question") {
-          additionalOffset = 500;
-        }
         window.scrollTo({
-          top: scrollPoint - additionalOffset,
+          top: scrollPoint + additionalOffset,
           behavior: "smooth",
         });
       }
-
       return false;
     });
   });
 });
 
-window.addEventListener('scroll',  headerFixed);
+window.addEventListener("scroll", headerFixed);
