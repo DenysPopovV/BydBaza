faf58500083267f59f29f17ec7a7fb86dba7027c
Denys <den.popov1002@gmail.com>
2023-09-25 13:59:44 +0300

    change

--- app/js/main.js
+++ app/js/main.js
@@ -28,7 +28,7 @@ const contactsForm = document.forms.contactForm,
 const patterns = {
   adressPattern: /[а-яА-ЯЁё0-9\s.,\-]{2,}/,
   textPattern: /[а-яА-ЯЁё]{2,}/,
-  namePattern: /^[а-яА-ЯҐґЄєІіЇї'-]{2,}$/,
+  namePattern: /^[а-яА-ЯҐґЄєІіЇї' -]{2,}$/,
   phonePattern: /^0\d{9}$/,
   emailPattern: /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/,
 };
